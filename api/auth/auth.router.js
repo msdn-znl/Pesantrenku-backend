4e84877dfd129f2bf3fc310911ab4b3c1ff6b2ac
Sirojuddin <nidu672@gmail.com>
2025-02-26 20:22:36 +0700

    feature: logout and refresh token

--- api/auth/auth.router.js
+++ api/auth/auth.router.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const passport = require('../../middleware/passport')
 const jwt = require('jsonwebtoken')
+const path = require('path')
+require('dotenv').config({path: path.join(__dirname, '../../.env')})
 
-router.post('/', async(req, res, next)=> {
+//login
+router.post('/login', async(req, res, next)=> {
     // eslint-disable-next-line no-unused-vars
     passport.authenticate("local", { session: false }, async (err, user, info) => {
       try{
@@ -24,22 +27,74 @@ router.post('/', async(req, res, next)=> {
             }
             
             const body = {id: user.id, username: user.username, role: user.roles}
-            const token = jwt.sign({user: body}, 'a9z1b8y2', {expiresIn: '15m' /** 15 menit */})
-            
+            const accessToken = jwt.sign({user: body}, process.env.ACCESS_SECRET, {expiresIn: '15m' /** 15 menit */})
+            const refreshToken = jwt.sign({user: body}, process.env.REFRESH_SECRET, {expiresIn: '1d' /** 1 hari */})
             //mengembalikan  token jwt
-            return res.status(200).json({token})
+            // return res.status(200).json({token})
 
             //mengembalikan token jwt ke http only cookie
-            // return res.cookie('token', token, {
-            //   httpOnly: true,
-            //   maxAge: 3600000 //1 Jam
-            // })
+            res.cookie('accessToken', accessToken, {
+              httpOnly: true,
+              secure: false,
+              sameSite: true,
+              maxAge: 900000 //15 Menit
+            })
+            res.cookie('refreshToken', refreshToken, {
+              httpOnly: true,
+              secure: false,
+              sameSite: true,
+              maxAge: 86400000 //1 Hari
+            })
+            return res.status(200).json({
+              status: 'success',
+              message: 'Login Success',
+            })
           }
-        )
+      )
       } catch (err){
         return next(err)
       }
     })(req, res, next)
+})
+
+//logout
+router.post('/logout', async (req, res, next)=> {
+  try {
+    res.cookie('accessToken', '', {httpOnly:true, secure: false, sameSite: 'Strict', expires: new Date(0)})
+    res.cookie('refreshToken', '', {httpOnly:true, secure: false, sameSite: 'Strict', expires: new Date(0)})
+    return res.status(200).json({
+      status: 'success',
+      message: 'Successfully loging out'
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+//refresh for access token
+router.post('/refresh', async(req, res, next) => {
+  try {
+    const refreshToken = req.cookies.refreshToken
+    if (!refreshToken) {
+      return res.status(401).json({status: 'failed', message: 'no token provided, please login'})
+    }
+    //cek apakah refresh token valid
+    jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, user) => {
+      if (err) return res.status(403).json({status: 'failed', message: 'invalid refresh token'})
+      //generate access token baru
+      const body = {id: user.id, username: user.username, role: user.roles}
+      const accessToken = jwt.sign(body, process.env.ACCESS_SECRET, {expiresIn: '15m'})
+      //mengirim access token ke cookie client
+      res.cookie('accessToken', accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: true,
+        maxAge: 900000 //15 menit
+      })
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
